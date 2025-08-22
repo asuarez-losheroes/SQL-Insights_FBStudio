adc5d3a72bf830c2619db7bf753507e07ed01755
Álvaro Suárez <asuarez.losheroes@gmail.com>
2025-08-22 16:11:23 +0000

    no descarta estos cambios, no se entendió, era por esta imagen, ahora se

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -95,7 +95,7 @@ export default function AdminLayout({
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
-              <Home className="h-6 w-6" />
+              <Image src="/logo.svg" width={32} height={32} alt="Logo" />
               <span className="">Inicio</span>
             </Link>
           </div>
